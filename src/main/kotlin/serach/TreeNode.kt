f2c5d8dff28ce42d81429420cbf4e09262b6807d
Kevin-Zahn <48159968+kev5655@users.noreply.github.com>
2024-05-20 18:23:49 +0200

    refactor treeNode

--- src/main/kotlin/serach/TreeNode.kt
+++ src/main/kotlin/serach/TreeNode.kt
@@ -1,26 +1,16 @@
 package serach
 
 class TreeNode<T>(
-    var value: T?,
-    var parent: TreeNode<T>? = null,
-    var left: TreeNode<T>? = null,
-    var right: TreeNode<T>? = null
+    private var value: T?,
+    private var parent: TreeNode<T>? = null,
+    private var left: TreeNode<T>? = null,
+    private var right: TreeNode<T>? = null
 ) {
-
     fun clear() {
         value = null
         parent = null
         left = null
         right = null
-    }
-}
-
-class BinaryTree<T> {
-
-    val root: TreeNode<T>? = null
-
-    fun createTree(list: List<T>) {
-        val sorted = list //.sorted()
     }
 }
 
